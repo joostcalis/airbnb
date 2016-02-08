a9417dfa91d6aba305f333042d692c620ef636fc
joostcalis <joost_calis@hotmail.com>
2016-02-08 15:42:10 +0100

    added new and create functions

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,4 +2,21 @@ class ListingsController < ApplicationController
   def index
     @listings = Listing.all
   end
+  def new
+    @listing = Listing.new
+  end
+  def create
+    if @listing = Listing.create(listing_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:title, :price, :location, :description)
+  end
 end
