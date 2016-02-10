fa1aa4e5711a6231c809c1bdb78322386d222e7e
joostcalis <joost_calis@hotmail.com>
2016-02-10 10:52:23 +0100

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    @user.build_guest
+    @user.build_host
   end
   def create
     if @user = User.create(user_params)
