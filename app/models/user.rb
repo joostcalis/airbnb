aa4b262c429b59ce04f0ae4d7fa2d6bb9970ce22
joostcalis <joost_calis@hotmail.com>
2016-02-10 13:24:07 +0100

    and again it should work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  has_one :host
-  has_one :guest
-  accepts_nested_attributes_for :guest
-  accepts_nested_attributes_for :host
+  has_many :listings
+  has_many :reservations
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210091414) do
+ActiveRecord::Schema.define(version: 20160210114200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "guests", force: :cascade do |t|
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "guests", ["user_id"], name: "index_guests_on_user_id", using: :btree
-
-  create_table "hosts", force: :cascade do |t|
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "hosts", ["user_id"], name: "index_hosts_on_user_id", using: :btree
 
   create_table "listings", force: :cascade do |t|
     t.string   "title"
@@ -39,23 +23,23 @@ ActiveRecord::Schema.define(version: 20160210091414) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "host_id"
     t.string   "image"
+    t.integer  "user_id"
   end
 
-  add_index "listings", ["host_id"], name: "index_listings_on_host_id", using: :btree
+  add_index "listings", ["user_id"], name: "index_listings_on_user_id", using: :btree
 
   create_table "reservations", force: :cascade do |t|
-    t.integer  "guest_id"
     t.integer  "listing_id"
     t.date     "start_date"
     t.date     "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
-  add_index "reservations", ["guest_id"], name: "index_reservations_on_guest_id", using: :btree
   add_index "reservations", ["listing_id"], name: "index_reservations_on_listing_id", using: :btree
+  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "firstname"
@@ -66,9 +50,7 @@ ActiveRecord::Schema.define(version: 20160210091414) do
     t.string   "email"
   end
 
-  add_foreign_key "guests", "users"
-  add_foreign_key "hosts", "users"
-  add_foreign_key "listings", "hosts"
-  add_foreign_key "reservations", "guests"
+  add_foreign_key "listings", "users"
   add_foreign_key "reservations", "listings"
+  add_foreign_key "reservations", "users"
 end
